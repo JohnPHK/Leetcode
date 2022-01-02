0f3c43eb4458a4e65527685bef0bdbe9dd56683b
Hokyun <hokyun.park@mail.utoronto.ca>
2022-01-02 18:01:29 -0500

    new solution

--- next_greater_element.py
+++ next_greater_element.py
@@ -1,16 +1,30 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        to_return = []
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    k = j + 1
-                    while k < len(nums2):
-                        if nums2[j] < nums2[k]:
-                            to_return.append(nums2[k])
-                            break
-                        k += 1
-                    if k == len(nums2):
-                        to_return.append(-1)
+from typing import List
 
-        return to_return
+def nextGreaterElement(nums1: List[int], nums2: List[int]) -> List[int]:
+    stack = []
+    ans = []
+    nums_map = {}
+    
+    for i in range(len(nums2)):
+        if len(stack) > 0:
+            p = stack[-1]
+            while p < nums2[i] and len(stack) > 0:
+                nums_map[p] = nums2[i]
+                stack.pop()
+                if len(stack) > 0:
+                    p = stack[-1]
+
+                
+        stack.append(nums2[i])
+    while len(stack) > 0:
+        p = stack.pop()
+        nums_map[p] = -1
+    
+    for i in range(len(nums1)):
+        if nums1[i] in nums_map:
+            ans.append(nums_map[nums1[i]])
+    
+    return ans
+
+if __name__ == "__main__":
+    print(nextGreaterElement([4,1,2], [1,3,4,2]))
