3aa6f253c9483a239bb622dfb53a3b3c47ee3e24
Hokyun <hokyun.park@mail.utoronto.ca>
2022-01-27 23:44:43 -0500

    test

--- perfect_number.py
+++ perfect_number.py
@@ -1,14 +1,13 @@
-class Solution:
-    def checkPerfectNumber(self, num: int) -> bool:
-        if num == 1:
-            return False
-        divisors = [1]
-        for i in range(2, int(num**0.5)+1):
-            if num%i == 0:
-                divisors.append(i)
-                divisors.append(num//i)
-        
-        return sum(divisors) == num
+def checkPerfectNumber(num: int) -> bool:
+    if num <= 1:
+        return False
+    divisors = [1]
+    for i in range(2, int(num**0.5)+1):
+        if num%i == 0:
+            divisors.append(i)
+            divisors.append(num//i)
+    
+    return sum(divisors) == num
 
 if __name__ == "__main__":
     print(checkPerfectNumber(6))
