85b13eb1236db32ed15b7d053ee7de4a117e1260
John Park <park.hokyun@gmail.com>
2022-03-31 03:16:10 -0400

    daily practice

--- gray_code.py
+++ gray_code.py
@@ -1,16 +1,12 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        def grayCodeHelper(result, k):
-            if k == 0:
-                result.append(0)
-                return
-            
-            grayCodeHelper(result, k - 1)
-            mask = 1 << (k-1)
-            for i in range(len(result) - 1, -1, -1):
-
-                result.append(result[i] | mask)
         
-        res = []
-        grayCodeHelper(res,n)
+        res = [0]
+        
+        for i in range(1, n+1):
+            mask = 1 << (i-1)
+            for j in range(len(res) - 1, -1, -1):
+
+                res.append(res[j] | mask)
+        
         return res
